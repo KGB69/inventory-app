b5ea99c33ce40bd124c604b04acfd69b31d9bf8b
KGB69 <KGBone@protonmail.com>
2025-07-02 14:03:28 +0300

    Add database setup with Flask-SQLAlchemy and InventoryItem model

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inventory.db'
+db = SQLAlchemy(app)
+
+class InventoryItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Item %r>' % self.name
 
 @app.route('/')
 def hello_world():
     return 'Hello, from your inventory management system!'
+
+@app.route('/init_db')
+def init_db():
+    with app.app_context():
+        db.create_all()
+    return 'Database tables created!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
