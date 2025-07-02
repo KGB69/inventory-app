97470103080552c0e0f9d85eb4d12705a804bca4
KGB69 <KGBone@protonmail.com>
2025-07-02 14:09:06 +0300

    Add CRUD operations and HTML template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -14,8 +14,32 @@ class InventoryItem(db.Model):
         return '<Item %r>' % self.name
 
 @app.route('/')
-def hello_world():
-    return 'Hello, from your inventory management system!'
+def index():
+    items = InventoryItem.query.all()
+    return render_template('index.html', items=items)
+
+@app.route('/add', methods=['POST'])
+def add_item():
+    name = request.form['name']
+    quantity = request.form['quantity']
+    new_item = InventoryItem(name=name, quantity=quantity)
+    db.session.add(new_item)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/update/<int:item_id>', methods=['POST'])
+def update_item(item_id):
+    item = InventoryItem.query.get_or_404(item_id)
+    item.quantity = request.form['quantity']
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:item_id>', methods=['POST'])
+def delete_item(item_id):
+    item = InventoryItem.query.get_or_404(item_id)
+    db.session.delete(item)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 @app.route('/init_db')
 def init_db():
